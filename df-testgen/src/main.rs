bb8e58b23d17dbbe5348cccd18cf3ded4277fccb
Ellen Arteca <ellen.arteca@gmail.com>
2021-12-27 11:53:19 -0500

    about to start on the discovery phase

--- df-testgen/src/main.rs
+++ df-testgen/src/main.rs
@@ -19,6 +19,14 @@ struct Opt {
     /// number of tests to generate
     #[structopt(long)]
     num_tests: i32,
+
+    /// running the discovery phase? default: no if there is an existing discovery output file
+    #[structopt(long)]
+    run_discover: bool,
+
+    /// running the test generation phase? default: yes
+    #[structopt(long)]
+    skip_testgen: bool,
 }
 
 fn main() {
@@ -29,6 +37,8 @@ fn main() {
                      .arg(&opt.lib_name)
                      .output()
                      .expect(format!("failed to execute API info gathering process for {:?}", &opt.lib_name).as_str());
+
+    let num_tests = opt.num_tests;
 
     println!("{:?}", opt);
 }
